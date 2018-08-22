9694d8714ce01462ae57684eb17599df534c5125
Koji Osugi <koji097@gmail.com>
2018-08-22 15:36:46 -0300

    Endpoint/pedidos/{id} disponibilizado

---
+++ src/main/java/br/com/cursospring/services/PedidoService.java
@@ -0,0 +1,22 @@
+package br.com.cursospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cursospring.domain.Pedido;
+import br.com.cursospring.repositories.PedidoRepository;
+import br.com.cursospring.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repository;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    "Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
