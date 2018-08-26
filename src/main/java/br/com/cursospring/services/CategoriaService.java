85430e9949dbcfb70170099bada0193f682624d8
Koji Osugi <koji097@gmail.com>
2018-08-26 20:28:14 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/br/com/cursospring/services/CategoriaService.java
+++ src/main/java/br/com/cursospring/services/CategoriaService.java
@@ -33,8 +33,9 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repository.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repository.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -58,5 +59,9 @@ public class CategoriaService {
 	
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+	
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
