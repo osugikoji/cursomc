c309da875222ffa86d58f4b00ae5b1fba32a44b2
Koji Osugi <koji097@gmail.com>
2018-09-02 00:31:39 -0300

    Email HTML

--- src/main/java/br/com/cursospring/services/EmailService.java
+++ src/main/java/br/com/cursospring/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.cursospring.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.cursospring.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
