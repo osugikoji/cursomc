0f4078191f4e9af547bf4df38739e5e452f0115b
Koji Osugi <koji097@gmail.com>
2018-09-05 23:24:48 -0300

    Adicionando senha a Cliente

--- src/main/java/br/com/cursospring/dto/ClienteNewDTO.java
+++ src/main/java/br/com/cursospring/dto/ClienteNewDTO.java
@@ -27,6 +27,9 @@ public class ClienteNewDTO implements Serializable {
 	private Integer tipo;
 	
 	@NotEmpty(message = "Preenchimento obrigatorio")
+	private String senha;
+	
+	@NotEmpty(message = "Preenchimento obrigatorio")
 	private String logradouro;
 	
 	@NotEmpty(message = "Preenchimento obrigatorio")
@@ -155,4 +158,13 @@ public class ClienteNewDTO implements Serializable {
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 }
+
