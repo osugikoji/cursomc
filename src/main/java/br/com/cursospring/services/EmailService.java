859919b3a52069676e5fee805d9f6925d168bfe2
Koji Osugi <koji097@gmail.com>
2018-09-16 18:43:41 -0300

    Esqueci a senha

--- src/main/java/br/com/cursospring/services/EmailService.java
+++ src/main/java/br/com/cursospring/services/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import br.com.cursospring.domain.Cliente;
 import br.com.cursospring.domain.Pedido;
 
 public interface EmailService {
@@ -11,6 +12,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
