9aaa1a5f4f85a05b57e3d577ff04af3d1267d0d2
Koji Osugi <koji097@gmail.com>
2018-09-16 22:49:04 -0300

    Enviando imagem via endpoint

--- src/main/java/br/com/cursospring/CursoSpringApplication.java
+++ src/main/java/br/com/cursospring/CursoSpringApplication.java
@@ -1,17 +1,11 @@
 package br.com.cursospring;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.cursospring.services.S3Service;
-
 @SpringBootApplication
 public class CursoSpringApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
@@ -19,6 +13,5 @@ public class CursoSpringApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\koji_\\Pictures\\Saved Pictures\\sunset.jpg");
 	}
 }
--- src/main/java/br/com/cursospring/resources/ClienteResource.java
+++ src/main/java/br/com/cursospring/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.cursospring.domain.Cliente;
@@ -84,5 +85,11 @@ public class ClienteResource {
 		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping("/picture")
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file) {
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 	}
 }
